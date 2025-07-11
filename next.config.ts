6cbf570c4c307c69c905e18123898b6a7e97e230
Maxwell Adesina <maxymax1886@gmail.com>
2025-07-11 00:43:18 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 's.yimg.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
