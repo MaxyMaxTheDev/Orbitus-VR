9c4563a97df22ebfdae6d51ef1d2363e3723ee1e
Maxwell Adesina <maxymax1886@gmail.com>
2026-04-08 00:42:16 +0000

    make the minecraft use https://prismarine-e91ajc481-zaro.vercel.app/

--- src/components/apps/minecraft.tsx
+++ src/components/apps/minecraft.tsx
@@ -5,7 +5,7 @@ export function MinecraftApp() {
   return (
     <div className="w-full h-full bg-black">
       <iframe
-        src="https://mcraft.fun"
+        src="https://prismarine-e91ajc481-zaro.vercel.app/"
         className="w-full h-full border-0"
         title="Minecraft"
         sandbox="allow-scripts allow-same-origin allow-forms"
