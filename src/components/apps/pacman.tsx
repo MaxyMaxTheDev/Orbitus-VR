7f677b557659199d7e3cdbda218e436b27701429
Maxwell Adesina <maxymax1886@gmail.com>
2026-04-09 22:55:47 +0000

    make pac man use the html from https://raw.githubusercontent.com/MaxyMax

--- src/components/apps/pacman.tsx
+++ src/components/apps/pacman.tsx
@@ -5,7 +5,7 @@ export function PacmanApp() {
     <div className="w-full h-full bg-black flex items-center justify-center">
       <div className="relative w-full max-w-[600px] aspect-[4/3] shadow-2xl shadow-yellow-500/10">
         <iframe
-          src="https://macek.github.io/google_pacman/"
+          src="https://maxymaxthedev.github.io/pacman/"
           className="w-full h-full border-0 rounded-lg"
           title="PAC-MAN"
           sandbox="allow-scripts allow-same-origin allow-forms"
